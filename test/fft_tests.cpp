87131e0f0c3c0cc973235ac7dd5bb450ac4386c3
benkrig <ben@sooryen.com>
2018-05-01 14:30:49 -0700

    Added test image deletion to teardown.

--- test/fft_tests.cpp
+++ test/fft_tests.cpp
@@ -17,10 +17,14 @@ protected:
 public:
     cv::Mat emptyMat;
     char * output_file_name = (char*)("../../Gray_img.JPG");
+    char * box = (char*)("../../square1.jpg");
+    char * boat = (char*)("../../img.JPG");
+
 };
 
 TEST_F(fftFixture, basicTest) {
     fft fft;
+
     std::vector<std::complex<double>> data = { 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0 };
 
     // forward fast_fourier_transform
@@ -45,31 +49,34 @@ TEST_F(fftFixture, basicTest) {
 TEST_F(fftFixture, loadImageTest){
     fft fft;
     cv::Mat image;
-    EXPECT_EQ(1, fft.load_image("../../img.JPG", image));
+    EXPECT_EQ(1, fft.load_image(boat, image, 256, 256));
 }
 
-TEST_F(fftFixture, displaImageTest){
+TEST_F(fftFixture, displayImageTest){
     fft fft;
     cv::Mat image;
-    EXPECT_EQ(1, fft.load_image("../../img.JPG", image));
-    fft.display_image(image);
-
+    EXPECT_EQ(1, fft.load_image(boat, image, 256, 256));
+    EXPECT_NO_THROW(fft.display_image(image));
 }
 
 TEST_F(fftFixture, saveImageTest){
     fft fft;
     cv::Mat image;
-    fft.load_image("../../img.JPG", image);
 
+    // Load a given image
+    fft.load_image(boat, image, 256, 256);
+
+    // Save image, 1 for success
     EXPECT_EQ(1, fft.save_image(output_file_name, image));
 
-    EXPECT_EQ(1, fft.load_image(output_file_name, image));
+    // Load the image we just saved, expect 1 for success
+    EXPECT_EQ(1, fft.load_image(output_file_name, image, 256, 256));
 }
 
 TEST_F(fftFixture, convertGray){
     fft fft;
     cv::Mat image;
-    EXPECT_EQ(1, fft.load_image("../../img.JPG", image));
+    EXPECT_EQ(1, fft.load_image(boat, image, 256, 256));
 
     // test valid image given and display the gray result
     EXPECT_EQ(1, fft.rgb_to_gray(image, image));
@@ -77,4 +84,26 @@ TEST_F(fftFixture, convertGray){
 
     // test empty image given
     EXPECT_EQ(0, fft.rgb_to_gray(emptyMat, emptyMat));
+}
+
+TEST_F(fftFixture, fftForwardTest){
+    fft fft;
+    cv::Mat image;
+    EXPECT_EQ(1, fft.load_image(boat, image, 256, 256));
+
+    // test valid image given and display the gray result
+    EXPECT_EQ(1, fft.rgb_to_gray(image, image));
+
+
+    std::vector<std::complex<double>>  x[256];
+    fft.real_to_complex_matrix(image, x);
+    fft.fft2d(x, 256, 256, 1);
+
+    // to reverse uncomment this line
+    //fft.fft2d(x, 256, 256, -1);
+
+    cv::Mat fourier_image;
+    fft.complex_matrix_to_mat(x, fourier_image, 256, 256);
+    //fft.center_frequency_on_mat(fourier_image);
+    fft.display_image(fourier_image);
 }
